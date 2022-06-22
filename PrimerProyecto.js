1fdcebf222cc3b774a762b5d5327c17fe68afa71
xurxobello <xurxobello@hotmail.es>
2022-06-22 20:33:12 +0200

    Funcion click en JS

--- PrimerProyecto.js
+++ PrimerProyecto.js
@@ -4,6 +4,9 @@ let colour = document.querySelector(".colour")
 let divs = document.querySelectorAll(".cube")
 let distance = -15
 let i = 0
+let win = document.querySelector(".win")
+let lose = document.querySelector(".lose")
+
 
 function generateNumber() {
     return Math.round(Math.random() * (255 - distance) + distance);
@@ -12,9 +15,8 @@ function generateNumber() {
 let number1 = generateNumber()
 let number2 = generateNumber()
 let number3 = generateNumber()
-console.log(number1);
-console.log(number2);
-console.log(number3);
+console.log(number1, number2, number3);
+
 
 function colorRGB() {
     let color = `(${number1}, ${number2}, ${number3})`
@@ -36,4 +38,15 @@ function colorRGB2(i) {
 for (const el of divs) {
     el.style.background = colorRGB2(i);
     i += 5
+}
+
+for (const el of divs) {
+    const divClickHandler = () => {
+        if (el.style.background === colour.style.background) {
+            win.textContent++
+        } else {
+            lose.textContent++
+        }
+    }
+    el.addEventListener("click", divClickHandler)
 }
